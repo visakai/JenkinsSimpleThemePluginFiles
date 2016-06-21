1988bf8463a4248ed7944290c57095dd1e36fd82
Sikai Peng <sikpeng@cisco.com>
2016-06-21 15:21:44 -0400

    document.observe loaded

--- simpleTheme.js
+++ simpleTheme.js
@@ -1,6 +1,4 @@
-(function() {
-  console.log("Hi from closure");
-  //var content = $("form[name='login']").innerHTML;
-  var len = document.getElementsByName("login").length;
-  console.log(len);
-})();
+document.observe('dom:loaded', function(){
+ console.log("loaded");
+ console.log($$("form[name='login']").first().innerHTML);
+});
